f0180d65b9d74e43720ec8e38dd0f71dffd8916a
derciosinione <derciosinione@gmail.com>
2021-10-17 18:46:00 +0100

    feat: Created Products Repository

--- src/Services/Catalog/Catalog.Api/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.Api/Repositories/ProductRepository.cs
@@ -54,9 +54,13 @@ namespace Catalog.Api.Repositories
             return (updateResult.IsAcknowledged && updateResult.ModifiedCount > 0);
         }
 
-        public Task<bool> DeleteProduct(string id)
+        public async Task<bool> DeleteProduct(string id)
         {
-            throw new System.NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context.Products.DeleteOneAsync(filter);
+
+            return (deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0);
         }
 
     }
